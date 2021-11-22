7e07b9c0fd70bcbe0909926a817f9283a13e65ad
ISazhyna <seoirinas@gmail.com>
2021-11-22 13:06:51 -0600

    added buttons

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from PIL import ImageTk, Image
 # ---------------------------- CONSTANTS ------------------------------- #
 
 YELLOW = "#f7f5dd"
+GREEN = "#9bdeac"
 FONT_NAME = "Courier"
 
 # ---------------------------- UI ------------------------------- #
@@ -14,9 +15,17 @@ window.config(padx=100, pady=50, bg=YELLOW)
 
 canv = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 
+timer=Label(text="Timer", font=(FONT_NAME, 30,"bold"), fg=GREEN, bg=YELLOW)
+timer.grid(column=1, row=0)
+
+start=Button(text="Start", font=(FONT_NAME, 20))
+start.grid(column=0, row=2)
+start=Button(text="Reset", font=(FONT_NAME, 20))
+start.grid(column=2, row=2)
+
 img = ImageTk.PhotoImage(Image.open("tomato.png")) # PIL
 canv.create_image(100, 112, image = img)
 canv.create_text(100,112,text="00:00",fill="white",font=(FONT_NAME,35,"bold"))
-canv.pack()
+canv.grid(column=1, row=1)
 
 window.mainloop()
